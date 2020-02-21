394f1a952ec4bd7a55f484d6b7bfd844fe9d05e4
Toru Hisai <toru@torus.jp>
2020-02-21 13:56:39 +0900

    add Google Analytics tag

--- script.scm
+++ script.scm
@@ -64,7 +64,16 @@
               (src "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js")
               (integrity "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM")
               (crossorigin "anonymous"))
-             "")))
+             "")
+
+     ;; <!-- Global site tag (gtag.js) - Google Analytics -->
+     (script (@ (src "https://www.googletagmanager.com/gtag/js?id=UA-158830523-1"))
+             "  window.dataLayer = window.dataLayer || [];"
+             "  function gtag(){dataLayer.push(arguments);}"
+             "  gtag('js', new Date());"
+             "  gtag('config', 'UA-158830523-1');"
+             )
+     ))
   )
 
 
